984d4ed392708507a4c1cd4a669262b3f5dd230a
VasiliskBRO <125855962+VasiliskBRO@users.noreply.github.com>
2025-02-13 10:29:55 +0500

    Add files via upload

---
+++ Untitled-1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+apt update && apt install -y openssh-server
+useradd -m -u 1010 -s /bin/bash sshuser
+echo "parol dla polzovatela sshuser:"
+passwd sshuser
+apt update && apt install -y sudo
+echo "sshuser ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/sshuser
+chmod 440 /etc/sudoers.d/sshuser
+su - sshuser -c "sudo apt update"
+SSH_DIR="/home/sshuser/.ssh"
+AUTHORIZED_KEYS="$SSH_DIR/authorized_keys"
+mkdir -p "$SSH_DIR"
+chmod 700 "$SSH_DIR"
+touch "$AUTHORIZED_KEYS"
+chmod 600 "$AUTHORIZED_KEYS"
+chown -R sshuser:sshuser "$SSH_DIR"
+SSHD_CONFIG="/etc/ssh/sshd_config"
+ISSUE_NET="/etc/issue.net"
+echo "Port 2024" | tee -a "$SSHD_CONFIG"
+echo "AllowUsers sshuser" | tee -a "$SSHD_CONFIG"
+echo "Authorized access only" | tee "$ISSUE_NET"
+echo "Banner $ISSUE_NET" | tee -a "$SSHD_CONFIG"
+systemctl restart sshd
