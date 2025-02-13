550bb31677e1a987fa7f4134281833fe21aa961c
VasiliskBRO <125855962+VasiliskBRO@users.noreply.github.com>
2025-02-13 10:18:50 +0500

    Delete Untitled-1.sh

--- Untitled-1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-useradd -m -u 1010 -s /bin/bash sshuser
-echo "parol dla polzovatela sshuser:"
-passwd sshuser
-apt update && apt install -y sudo
-echo "sshuser ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/sshuser
-chmod 440 /etc/sudoers.d/sshuser
-su - sshuser -c "sudo apt update"
