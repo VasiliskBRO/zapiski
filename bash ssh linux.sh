080f631abbc3fc47c2952074f8954e51884f3ea8
VasiliskBRO <125855962+VasiliskBRO@users.noreply.github.com>
2025-04-18 12:30:00 +0500

    Update bash ssh linux.sh

--- bash ssh linux.sh
+++ bash ssh linux.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt update && apt install -y openssh-server
-useradd -m -u 1010 -s /bin/bash sshuser
+sudo useradd -m -u 1010 -s /bin/bash sshuser
 echo "parol dla polzovatela sshuser:"
-passwd sshuser
+sudo passwd sshuser
 apt update && apt install -y sudo
 echo "sshuser ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/sshuser
-chmod 440 /etc/sudoers.d/sshuser
-su - sshuser -c "sudo apt update"
+sudo chmod 440 /etc/sudoers.d/sshuser
+sudo su - sshuser -c "sudo apt update"
 SSH_DIR="/home/sshuser/.ssh"
 AUTHORIZED_KEYS="$SSH_DIR/authorized_keys"
 mkdir -p "$SSH_DIR"
@@ -20,4 +20,4 @@ echo "Port 2024" | tee -a "$SSHD_CONFIG"
 echo "AllowUsers sshuser" | tee -a "$SSHD_CONFIG"
 echo "Authorized access only" | tee "$ISSUE_NET"
 echo "Banner $ISSUE_NET" | tee -a "$SSHD_CONFIG"
-systemctl restart sshd
+sudo systemctl restart sshd
